b36f7a16a821a76b1c1eddca314eedeed909ba1a
Brandon <kounse@gmail.com>
2023-02-27 22:01:14 -0600

    Created methods for my_each and my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    if block_given?
+      my_each do |i|
+        yield(i, index)
+        index += 1
+      end
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +20,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for i in self
+      yield(i)
+    end
+    self
+  end
 end
